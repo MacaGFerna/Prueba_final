8245603000bafd3ef97a4d5314e666914140c9c6
Macarena Gabler <maca.skys@outlook.com>
2022-09-23 23:33:34 -0300

    ultimos detalles

--- assets/js/.js
+++ assets/js/.js
@@ -1,22 +1,15 @@
-$(document).ready(function(){
- 
-    $("a").on('click', function(event) {
-  
-  
-      if (this.hash !== "") {
-    
+$(function(){
+    $("a").click (function(event){
         event.preventDefault();
-  
-        var hash = this.hash;
-  
-    
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-  
-          
-          window.location.hash = hash;
+
+        var car = this.hash; 
+
+        $("html, body").animated({
+
+        }, 800, function (){
+            window.location.hash = car; 
         });
-      }
-    });
-  });
+
+     }
+)}
+) 
